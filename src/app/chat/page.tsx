5fc548a222bcbbeffbd642b0e12f33233af9d395
rogerjeasy <rogerjeasy@gmail.com>
2025-03-17 23:15:57 +0100

    chat component page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ChatContainer from '@/components/chat/ChatContainer';
+import ChatLayout from '@/components/chat/ChatLayout';
+
+export default function ChatPage() {
+  return (
+    <ChatLayout>
+      <ChatContainer />
+    </ChatLayout>
+  );
+}
