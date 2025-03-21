a377ae71f6c34afbbf4072b41b9ba428621f9dd3
rogerjeasy <rogerjeasy@gmail.com>
2025-03-21 03:34:30 +0100

    changed the version of react-date-picker

--- src/app/application-management/page.tsx
+++ src/app/application-management/page.tsx
@@ -1,5 +1,11 @@
 // src/app/application-management/page.tsx
+import { Metadata } from 'next'
 import ApplicationManagementMainComponent from "@/components/application-management/ApplicationManagementMainComponent";
+
+export const metadata: Metadata = {
+    title: 'Application Management | HSLU Data Science Exam Preparation',
+    description: 'Manage users, courses, FAQs, and files for the HSLU Data Science Exam Preparation Assistant',
+    }
 
 export default function ApplicationManagementPage() {
   return (
